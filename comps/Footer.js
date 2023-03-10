b91d606d00b6ad6f2b637e5d8bd4f3d8b111a17b
Ulises Reyes <urjr@gmail.com>
2023-03-10 16:33:05 -0500

    added component folder

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+           <i>Copyright 2023 Ulises</i>
+        </div>
+    );
+}
+ 
+export default Footer;
---
+++ comps/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>UlisesNav</h1>
+            <a>Home</a>
+            <a>About</a>
+            <a>Ninja Listing</a>
+        </nav>
+    );
+}
+ 
+export default Navbar;
