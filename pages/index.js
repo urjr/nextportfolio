eafa8ff7033eba2d4d09e21485fdd10e0205ab63
Ulises Reyes <urjr@gmail.com>
2023-03-10 16:32:07 -0500

    added navbar and footer components

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/comps/Navbar'
+import Footer from '@/comps/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +11,11 @@ export default function Home() {
   return (
     <>
       <div>
+        <Navbar />
         <h1>Hello World</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
+        <Footer />
       </div>
     </>
   )
