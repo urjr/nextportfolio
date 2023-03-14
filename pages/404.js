bb1933529ad1fe9e1faba51ccf25d380073c367f
Ulises Reyes <urjr@gmail.com>
2023-03-14 12:39:01 -0400

    Added redirect to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,21 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link';
+
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect (() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, []);
+
     return (
-        <h1>
-            Sorry....
-        </h1>
+        <>
+            <h1>Sorry....</h1>
+            <p>You will be redirected in a few seconds. If not, click <Link href="/">Here</Link></p>
+        </>
     );
 }
  
