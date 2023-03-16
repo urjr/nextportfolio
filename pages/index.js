9e5842c525cc750a21253ab125476c101579f8eb
Ulises Reyes <urjr@gmail.com>
2023-03-16 13:53:17 -0400

    removed @next/font and added font styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/comps/Navbar'
 import Footer from '@/comps/Footer'
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Ulises&apos; Homepage</title>
       </Head>
       <div>
-        <h1>Hello World</h1>
+        <h1 className={styles.header + " " + inter.className}>Hello World</h1>
         <p className={styles.lorem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
         <p className={styles.lorem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nunc dolor, malesuada sit amet nisl in, rhoncus pharetra ipsum. Praesent sed placerat ante, eu porttitor.</p>
         <Link href="/projects">See Project List</Link>
